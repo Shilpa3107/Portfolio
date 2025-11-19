74fa658653a54c10fac200091d8be833820bdc7c
Shilpa <shilpa1work@gmail.com>
2025-11-19 19:49:17 +0000

    where is my image. there is no image in about me section

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -11,7 +11,7 @@ export default function About({ about }: AboutProps) {
     <Section id="about" title="About Me" icon={<User className="h-6 w-6" />}>
       <div className="grid items-center gap-8 md:grid-cols-3">
         <div className="relative mx-auto h-48 w-48 overflow-hidden rounded-full md:h-64 md:w-64">
-           <Image
+          <Image
             src="https://storage.googleapis.com/prompt-gallery/prod/9821a711-f2f5-465d-9b55-41275d27553f/image.jpeg"
             alt="Shilpa Sinha"
             width={400}
